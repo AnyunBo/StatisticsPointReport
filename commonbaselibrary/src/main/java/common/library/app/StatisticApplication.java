266240aca19d21d9b91230aefdeb108a19513351
bingbo <peibingbo@secoo.com>
2021-12-01 14:35:14 +0800

    添加项目application基础配置

---
+++ commonbaselibrary/src/main/java/common/library/app/StatisticApplication.java
@@ -0,0 +1,25 @@
+package common.library.app;
+
+import common.library.base.BaseApplication;
+import com.library.base.BuildConfig;
+
+public class StatisticApplication extends BaseApplication {
+
+    public static String CHANNEL_ID = BuildConfig.isBetaChannel ? "test" : "vivo";    //	渠道号
+    //大数据埋点的索引值
+    public static int RECORD_INDEX = 0;
+    private static StatisticApplication instance;
+
+    public static StatisticApplication getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RECORD_INDEX = 0;
+        instance = this;
+    }
+
+
+}
